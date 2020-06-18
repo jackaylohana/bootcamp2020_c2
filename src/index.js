ab67f105ca6fe195c0a57c080e6cd37e467a43e1
jackaylohano <jackaylohana@gmail.com>
2020-06-18 12:45:07 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>Hello Star stuff!</div>
   </React.StrictMode>,
   document.getElementById('root')
 );
